72c1b6955d16576c13332f56f3b888a834c619fb
KURUVA UDAY <udaykiran.kuk70@gmail.com>
2026-06-11 09:01:26 +0530

    Implement leave application workflow
    - Added leave request module
    - Added employee-leave relationship
    - Added leave validations
    - Added leave balance checks
    - Added apply leave API

--- src/main/java/com/uday/leave_management/service/LeaveRequestService.java
+++ src/main/java/com/uday/leave_management/service/LeaveRequestService.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Service;
 @Service
 public interface LeaveRequestService {
     LeaveResponseDto applyLeave(Long employeeId, LeaveRequestDto requestDto);
-    LeaveResponseDto approveLeave(Long leaveRequestId){
+    LeaveResponseDto approveLeave(Long leaveRequestId);
 
-    }
+
 }
